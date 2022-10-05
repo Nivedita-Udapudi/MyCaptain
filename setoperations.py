3950e7ddc05dfb987659b13c6f6d406a4d6a718e
Nivedita_LU_3327 <97021393+Nivedita-Udapudi@users.noreply.github.com>
2022-10-05 13:41:24 +0530

    Created setoperations.py
    This program performs the set operations like union, intersection, difference and symmetric difference.

---
+++ setoperations.py
@@ -0,0 +1,13 @@
+s1={1,2,3,4,5}
+s2={2,4,6,7,8}
+union=s1.union(s2)
+print(union)
+
+intersection=s1.intersection(s2)
+print(intersection)
+
+difference=s1.difference(s2)
+print(difference)
+
+sym_difference=s1.symmetric_difference(s2)
+print(sym_difference)
